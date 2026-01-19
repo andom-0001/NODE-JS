e7732d1406e7fe5b2200c0682212c590561c58bb
andom-0001 <anandkumar241103@gmail.com>
2026-01-19 14:26:12 +0530

    node js sectionz index.js

--- sectionz/index.js
+++ sectionz/index.js
@@ -3,9 +3,16 @@ const http = require('node:http');
 const hostname = '127.0.0.1';
 const port = 3000;
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Welcome to Node js with http module\n');
+    if (req.url === '/') {
+        res.write("this is my main home page");
+    } else if (req.url === "about_us") {
+        res.write("this is my about us page");
+    } else if (req.url === "contact_us") {
+        res.write("this is my contact us page");
+    } else {
+        res.write("404 page not found");
+    }
 });
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
