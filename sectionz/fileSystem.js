67b8fbd6ee1cd2cd51aaa897e442876b09f2f51c
andom-0001 <anandkumar241103@gmail.com>
2026-01-27 09:17:52 +0530

    node js

--- sectionz/fileSystem.js
+++ sectionz/fileSystem.js
@@ -1,4 +1,7 @@
 let fs = require("fs").promises;
+let {promisify} = require("util");
+let AnacondaRex = promisify(require("fs").readFile);
+
 
 fs.writeFile("cb.txt", "This is simple call_back file data", (err) => {
     if (err) {
@@ -18,3 +21,13 @@ async function readFileWithPromises() {
 }
 
 readFileWithPromises();
+
+async function readFileWithPromisify() {
+    try {
+        let filedata = await AnacondaRex("cb.txt", "utf-8");
+        console.log("promisify file data", filedata);
+    } catch (err) {
+        console.log("promisify file is loading error", err);
+    }
+}
+readFileWithPromisify();
